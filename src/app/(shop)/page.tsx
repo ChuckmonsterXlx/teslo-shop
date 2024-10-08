2040004d2b5507123897b6ae450d8c59505c55e4
ChuckmonsterXlx <alfonso_309@hotmail.com>
2024-10-07 22:59:05 -0700

    feat(products): added grid for products

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function HomePage() {
   return (
     <>
       <Title title="Store" subtitle="All products" />
+
+      <ProductGrid products={products} />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@
 export * from "./ui/top-menu/TopMenu";
 export * from "./ui/not-found/PageNotFound";
 export * from "./ui/title/Title";
+
+// products
+export * from "./products/product-grid/ProductGrid";
