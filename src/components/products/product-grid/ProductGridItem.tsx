25dd097910054f977f86a69000cba0784d89f09f
ChuckmonsterXlx <alfonso_309@hotmail.com>
2025-01-03 12:07:41 -0700

    feat: the image is changed

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,21 +1,29 @@
-import { Product } from "@/interfaces";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+import { Product } from "@/interfaces";
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
+
   return (
     <div className="overflow-hidden rounded-md fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${displayImage}`}
           alt={product.title}
-          className="object-cover w-full"
+          className="object-cover w-full rounded"
           width={500}
           height={500}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(product.images[0])}
         />
       </Link>
 
