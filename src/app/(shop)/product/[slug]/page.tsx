312707121cff00544ba77530f710e3690f3e864e
ChuckmonsterXlx <alfonso_309@hotmail.com>
2025-05-27 18:29:56 -0700

    feat: the product screen was added

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,47 @@
-export default function ProductPage() {
+import { notFound } from "next/navigation";
+import { titleFont } from "@/config";
+import { initialData } from "@/seed";
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    return notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-3 gap-3">
+      {/* Slideshow */}
+      <div className="col-span-1 md:col-span-2">
+        <h1>Product</h1>
+      </div>
+
+      {/* Product details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">{product.price}</p>
+
+        {/* Size selector */}
+
+        {/* Quantity selector */}
+
+        {/* Button */}
+        <button className="btn-primary my-5">Add to cart</button>
+
+        {/* Description */}
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
